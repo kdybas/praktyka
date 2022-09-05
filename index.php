4a815d3825f083d79048580fc057bb21bdf08118
kdybas <112871463+kdybas@users.noreply.github.com>
2022-09-05 14:30:52 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+	<meta charset="utf-8">
+	<meta content="width=device-width, initial-scale=1.0" name="viewport">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title></title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<div class="glowny" >
+
+	<div class="menu">
+			menu
+	</div> 
+<div class="tresc">
+	tresc
+	</div>	
+	</div>
+</body>
+</html>
+
+
+
+
+
+
