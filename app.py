b483ef5549a1127beb51f79d4a346ca43ddf89e4
kdybas <112871463+kdybas@users.noreply.github.com>
2022-09-19 15:43:39 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+@app.route('/test', methods=['GET'])
+def test():
+    if request.method == "GET":
+        return jsonify({"Response": "Get Request Called"})
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=9000)
+
+
